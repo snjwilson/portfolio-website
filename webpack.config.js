7d58db78eca5dbcaf0e5c620f96848c6c719ed47
Sanjay Wilson <sanjaywilsonktm@gmail.com>
2020-06-23 23:59:53 +0530

    [add] basic routes

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,16 @@
-const path = require("path");
+const path = require('path');
 
 const config = {
-  entry: "./lib/components/Index.js",
+  resolve: {
+    modules: [path.resolve('./lib'), path.resolve('./node_modules')],
+  },
+  entry: ['babel-polyfill', './lib/renderers/dom.js'],
   output: {
-    path: path.resolve(__dirname, "public"),
-    filename: "bundle.js",
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
   },
   module: {
-    rules: [{ test: /\.js$/, exclude: /node_modules/, use: "babel-loader" }],
+    rules: [{ test: /\.js$/, exclude: /node_modules/, use: 'babel-loader' }],
   },
 };
 
